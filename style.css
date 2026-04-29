9afb6728dc52b5f217f40dcb1441148175ea1778
AwesomeeDev <AwesomeDevv@proton.me>
2026-04-30 10:03:15 +1200

    Create style.css

---
+++ style.css
@@ -0,0 +1,31 @@
+body {
+  margin: 0;
+  font-family: Arial, sans-serif;
+  background: black;
+  color: gold;
+}
+
+section {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+/* Hidden by default */
+.reveal {
+  opacity: 0;
+  transform: translateY(50px);
+  transition: 1s ease;
+}
+
+/* When visible */
+.reveal.active {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.hero {
+  background: linear-gradient(black, darkred);
+}
